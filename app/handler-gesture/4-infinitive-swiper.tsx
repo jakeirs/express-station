999d3771338220cf482373ab3029c37bb4d6ea68
Simon <kierej@gmail.com>
2025-02-24 20:04:51 +0700

    asd

--- app/handler-gesture/4-infinitive-swiper.tsx
+++ app/handler-gesture/4-infinitive-swiper.tsx
@@ -17,8 +17,29 @@ export default function HandlerGesture() {
         <View style={{ backgroundColor: '#111dff', flex: 1 }} className="h-96 w-full p-2">
           <Text>Slide 4</Text>
         </View>
-        <View style={{ backgroundColor: '#7c4de9', flex: 1 }} className="h-96 w-full p-2">
+        <View style={{ backgroundColor: '#7ceee9', flex: 1 }} className="h-96 w-full p-2">
+          <Text>Slide 5</Text>
+        </View>
+        <View style={{ backgroundColor: '#7c4d', flex: 1 }} className="h-96 w-full p-2">
           <Text>Slide 6</Text>
+        </View>
+        <View style={{ backgroundColor: '#7ce9', flex: 1 }} className="h-96 w-full p-2">
+          <Text>Slide 7</Text>
+        </View>
+        <View style={{ backgroundColor: '#ec4de9', flex: 1 }} className="h-96 w-full p-2">
+          <Text>Slide 8</Text>
+        </View>
+        <View style={{ backgroundColor: '#7cdee9', flex: 1 }} className="h-96 w-full p-2">
+          <Text>Slide 9</Text>
+        </View>
+        <View style={{ backgroundColor: '#cccdec', flex: 1 }} className="h-96 w-full p-2">
+          <Text>Slide 10</Text>
+        </View>
+        <View style={{ backgroundColor: '#dd4d3d', flex: 1 }} className="h-96 w-full p-2">
+          <Text>Slide 11</Text>
+        </View>
+        <View style={{ backgroundColor: '#7c3c39', flex: 1 }} className="h-96 w-full p-2">
+          <Text>Slide 12</Text>
         </View>
       </InfiniteSwiper>
     </View>
